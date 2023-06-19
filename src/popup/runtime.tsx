9553580ee57e469c08d1d451ab94e2016615b953
huangqiuyun03 <huangqiuyun03@kuaishou.com>
2023-06-19 21:53:24 +0800

    feat(popup):change antd

--- src/popup/runtime.tsx
+++ src/popup/runtime.tsx
@@ -1,17 +1,12 @@
-import React, {useMemo, useCallback} from 'react';
+import React, {useMemo, useCallback, useRef, useEffect} from 'react';
 import { AlignEnum } from './constants';
+import { Button, Modal } from 'antd';
 
 import css from './runtime.less'
 
-export default function ({
-                           env,
-                           _env,
-                           data,
-                           slots,
-                           outputs
-                         }) {
-  
-  const isDebug = !!(env.runtime && env.runtime.debug)
+export default function ({env, _env, data, slots, outputs}) {
+  const ref = useRef<any>();
+
   const handleClose = useCallback(() => {
     _env.currentScenes.close()
   }, [])
@@ -22,17 +17,14 @@ export default function ({
 
   const handleOk = useCallback(() => {
     const okFn = outputs['ok']
-    const cons = okFn.getConnections()
-    if (cons.length === 0) {//没有连接则关闭当前场景
-      _env.currentScenes.close()
-    }
-
     okFn()////TODO 获取当前连接数
   }, [])
 
   const renderFooter = ()=>{
     return(
-      <div className={css.footer} data-toolbar style={{display: !data.useFooter ? 'none' : void 0, justifyContent: data.footerLayout || AlignEnum.FlexEnd }}>
+      <div
+        data-toolbar 
+        style={{ justifyContent: data.footerLayout || AlignEnum.FlexEnd, display: 'flex' }}>
         {(data.footerBtns || []).map((item) => {
           const {
             title,
@@ -41,65 +33,77 @@ export default function ({
             visible
           } = item;
           return (
-            <button
+            <Button
               onClick={id==='cancel' ? handleCancel : handleOk}
-              className={`${css.button} ${type==='default' ? css.buttonDefault: css.buttonPrimary}`}
               data-handler-button={id}
               key={id}
+              type = {type}
             >
               {env.i18n(title)}
-            </button>
+            </Button>
           );
         })}
       </div>
     )
   }
 
-  const jsx = (
+  //调试和发布态
+  const debugPopup = (
     <div 
-      className={`${css.popup} ${!data.centered && !env.edit  ? css.notCenter : void 0}`} 
-      style={{width: data.width}}>
-      <div className={css.content}>
-        <button className={css.close} onClick={handleClose}>
-          <span className={css.x}>
-            <span className={css.antion}>
-              <svg viewBox='64 64 896 896' focusable='false' data-icon='close' width='1em' height='1em'
-                   fill='currentColor' aria-hidden='true'><path
-                d='M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z'></path></svg>
-            </span>
-          </span>
-        </button>
+      className={css.debugMask} 
+      ref={ref}>
+        <Modal
+        open = {true}
+        title={data.hideTitle ? undefined : env.i18n(data.title)}
+        width={data.width}
+        footer={data.useFooter ? renderFooter() : null}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        centered={data.centered}
 
-        <div className={css.header}>
-          <div className={css.title} data-modle-title>
-            {data.hideTitle ? undefined : env.i18n(data.title)}
-          </div>
-        </div>
+        wrapClassName={css.container}
+        closable={data.closable}
+        getContainer={()=>{
+          if(ref){
+            return ref.current
+          }
+        }}>
+        {slots['body'].render()}
+        </Modal>
+    </div>
+  )
+  //编辑态
+  const editPopup = (
+    <div 
+      className={css.antdMask}
+      ref={ref}
+    >
+      <Modal
+        title={data.hideTitle ? undefined : env.i18n(data.title)}
+        width={data.width}
+        footer={data.useFooter ? renderFooter() : null}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        mask={false}
+        open = {true}
 
-        <div className={`${css.body} ${env.edit ? css.bodyEdit : ''}`} 
-          //背景色
-          //style={{...data.bodyStyle}}
-        >
+        wrapClassName={css.container}
+        closable={data.closable}
+        getContainer={false}>
           {slots['body'].render()}
-        </div>
-
-        {renderFooter()}
-      </div>
+      </Modal>
     </div>
   )
 
+  //非编辑态, 用antd
   if (!env.edit) {
     return (
-      // <div className={`${!data.centered && !env.edit ? css.notCenterMask : css.mask}`}>
-      <div className={`${isDebug ? css.debugMask : css.notCenterMask} ${data.centered ? css.center : '' }`}>
-        {jsx}
+      <div className={css.mask}>
+        {debugPopup}
       </div>
     )
   }
 
-  return jsx
-}
-
-function Popup() {
-
+  //编辑态，用手写的
+  return editPopup;
 }
