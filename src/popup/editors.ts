ac49d151b4de8da5647852be9f4ab5e9a186f6fd
huangqiuyun03 <huangqiuyun03@kuaishou.com>
2023-06-26 20:27:46 +0800

    feat(popup):popup improve

--- src/popup/editors.ts
+++ src/popup/editors.ts
@@ -1,5 +1,6 @@
 import { isEmptyString, uuid } from './utils';
-import { DefaultEvent, AlignEnum, Data, DialogButtonProps } from './constants';
+import visibleOpt from './utils'
+import { DefaultEvent, AlignEnum, Data, DialogButtonProps, Location } from './constants';
 
 function findConfig({ data, focusArea }, propKey?: string) {
   if (!focusArea) return;
@@ -12,57 +13,91 @@ function findConfig({ data, focusArea }, propKey?: string) {
   return data.footerBtns[index];
 }
 
-/**
- * 删除/隐藏按钮
- * @param env 上下文 
- * @param btnId 确认/取消按钮的默认ID
- */
-function removeBtn({ data, input, output, slot }:
-  { data: Data, input: any, output: any, slot: any },
-  btnId: string
-) {
-  const index = data.footerBtns?.findIndex(btn => btn.id === btnId);
-  output.remove(btnId);
-  if (!DefaultEvent.includes(btnId)) {
-    data.footerBtns.splice(index, 1);
-  }
-}
-
 //新增按钮操作
-function addBtn({ data, input, output, slot }: { data: Data, input: any, output: any, slot: any }, defaultId?: string) {
-  const { footerBtns } = data;
-  const titleMap = {
-    [DefaultEvent[0]]: '确认',
-    [DefaultEvent[1]]: '取消',
-  }
-  const id = defaultId || uuid(),
-    title = defaultId ? titleMap[defaultId] : `按钮${footerBtns.length + 1}`;
-  const schema = {
-    type: 'any'
-  };
+let btnsLength,
+    addBtn;
 
-  const defaultBtn: DialogButtonProps = {
-    id,
-    title,
-    icon: '',
-    dynamicHidden: true,
-    dynamicDisabled: true,
-    visible: true,
-    autoClose: true,
-    useIcon: false,
-    showText: true,
-    type: 'default',
-    isConnected: false
+const initParams = (data: Data, output) => {
+  btnsLength = (data.footerBtns || []).length;
+  addBtn = (btn) => {
+    data.footerBtns.unshift(btn);
+    output.add(btn.id, `点击${btn.title}`, { type: 'any' })
   };
-  
-  output.add(`点击${id}`, `点击${title}`, schema);
-  output.add(id, title, schema);
-  //slot.get(SlotIds.Container).inputs.add(id, `${title}`, { type: 'any' });
-  //slot.get('body').outputs.add(id, `${title}`, { type: 'follow' });
+};
 
-  if (!DefaultEvent.includes(id)) {
-    data.footerBtns.unshift(defaultBtn);
-  }
+//图标编辑
+function icon(dataset: string) {
+  return {
+    title: '图标',
+    items: [
+      {
+        title: '使用图标',
+        type: 'Switch',
+        value: {
+          get({ data, focusArea }: EditorResult<Data>) {
+            return findConfig({ data, focusArea }).useIcon;
+          },
+          set({ data, focusArea }: EditorResult<Data>, value: boolean) {
+            const res = findConfig({ data, focusArea }, 'icon');
+            if (!res?.length) {
+              findConfig({ data, focusArea }).icon = 'HomeOutlined';
+            }
+            findConfig({ data, focusArea }).useIcon = value;
+          }
+        }
+      },
+      {
+        title: '显示文字',
+        type: 'Switch',
+        ifVisible({ data, focusArea }: EditorResult<Data>) {
+          const useIcon = findConfig({ data, focusArea }, 'useIcon');
+          return useIcon ? true : false;
+        },
+        value: {
+          get({ data, focusArea }: EditorResult<Data>) {
+            return findConfig({ data, focusArea }, 'showText');
+          },
+          set({ data, focusArea }: EditorResult<Data>, value: boolean) {
+            findConfig({ data, focusArea }).showText = value;
+          }
+        }
+      },
+      {
+        title: '图标位置',
+        type: 'Select',
+        options: [
+          { label: '位于文字前', value: Location.FRONT },
+          { label: '位于文字后', value: Location.BACK }
+        ],
+        ifVisible({ data, focusArea }) {
+          const useIcon = findConfig({ data, focusArea }, 'useIcon');
+          return useIcon ? true : false;
+        },
+        value: {
+          get({ data, focusArea }: EditorResult<Data>) {
+            return findConfig({ data, focusArea }, 'location') || Location.FRONT;
+          },
+          set({ data, focusArea }: EditorResult<Data>, value: boolean) {
+            findConfig({ data, focusArea }).location = value;
+          }
+        }
+      },
+      {
+        type: 'Icon',
+        ifVisible({ data, focusArea }: EditorResult<Data>) {
+          return findConfig({ data, focusArea }, 'useIcon');
+        },
+        value: {
+          get({ data, focusArea }: EditorResult<Data>) {
+            return findConfig({ data, focusArea }, 'icon');
+          },
+          set({ data, focusArea }: EditorResult<Data>, value: string) {
+            findConfig({ data, focusArea }).icon = value;
+          }
+        }
+      }
+    ]
+  };
 }
 
 export default {
@@ -91,49 +126,36 @@ export default {
           }
         }
       },
-      // {
-      //   title: '内容高度限制',
-      //   description: '设置0为不限制，超出高度限制出现滚动条',
-      //   type: 'Slider',
-      //   options: {
-      //     max: 5000,
-      //     min: 0,
-      //     step: 100,
-      //     formatter: 'px'
-      //   },
-      //   value: {
-      //     get({ data }) {
-      //       return data.bodyStyle?.maxHeight;
-      //     },
-      //     set({ data }, value: string) {
-      //       if (!data.bodyStyle) {
-      //         data.bodyStyle = {};
-      //       }
-      //       data.bodyStyle = {
-      //         ...data.bodyStyle,
-      //         maxHeight: value || undefined
-      //       };
-      //     }
-      //   }
-      // },
-      // {
-      //   title: '内容背景色',
-      //   type: 'colorpicker',
-      //   value: {
-      //     get({ data }) {
-      //       return data.bodyStyle?.backgroundColor;
-      //     },
-      //     set({ data }, value: string) {
-      //       if (!data.bodyStyle) {
-      //         data.bodyStyle = {};
-      //       }
-      //       data.bodyStyle = {
-      //         ...data.bodyStyle,
-      //         backgroundColor: value
-      //       };
-      //     }
-      //   }
-      // }
+      {
+        title: '内容高度限制',
+        description: '设置0为不限制，超出高度限制出现滚动条',
+        type: 'Slider',
+        options: {
+          max: 5000,
+          min: 0,
+          step: 100,
+          formatter: 'px'
+        },
+        value: {
+          get({ data }) {
+            return data.bodyStyle?.maxHeight;
+          },
+          set({ data }, value: string) {
+            if (!data.bodyStyle) {
+              data.bodyStyle = {};
+            }
+            data.bodyStyle = {
+              ...data.bodyStyle,
+              maxHeight: value || undefined
+            };
+          }
+        }
+      },
+      {
+        title: '内容',
+        options: [ { type: 'background', config: { disableBackgroundImage: true } } ],
+        target: '.ant-modal-body'
+      }
     ],
     items: ({}, cate1, cate2) => {
       cate1.title = '常规';
@@ -189,38 +211,90 @@ export default {
           },
         },
         {
-          title: '工具条',
-          items: [
-            {
-              title: '显示',
-              type: 'switch',
-              value: {
-                get({ data }) {
-                  return data.useFooter;
-                },
-                set({ data }, value: boolean) {
-                  data.useFooter = value;
-                }
-              }
+          title: '点击蒙层关闭',
+          type: 'switch',
+          value: {
+            get({ data }) {
+              return !!data.maskClosable;
             },
-            // {
-            //   title: '新增操作',
-            //   ifVisible({ data }) {
-            //     return (
-            //       !!data.footerBtns &&
-            //       data.useFooter
-            //     );
-            //   },
-            //   type: 'Button',
-            //   value: {
-            //     set({ data, input, output, slot }) {
-            //       addBtn({ data, input, output, slot });
-            //     }
-            //   }
-            // }
-          ]
-        }
+            set({ data }, val: boolean) {
+              data.maskClosable = val;
+            }
+          }
+        },
       ];
+      cate2.title = '操作区';
+      cate2.items = [
+        {
+          title: '显示',
+          type: 'switch',
+          value: {
+            get({ data }) {
+              return data.useFooter;
+            },
+            set({ data }, value: boolean) {
+              data.useFooter = value;
+            }
+          }
+        },
+        {
+          title: '对齐方式',
+          type: 'Radio',
+          options: [
+            { value: AlignEnum.FlexStart, label: '居左' },
+            { value: AlignEnum.Center, label: '居中' },
+            { value: AlignEnum.FlexEnd, label: '居右' }
+          ],
+          value: {
+            get({ data }) {
+              return data.footerLayout;
+            },
+            set({ data }, value: AlignEnum) {
+              data.footerLayout = value;
+            }
+          }
+        },
+        {
+          title: '操作列表',
+          description: '选中拖拽各项左侧手柄，可改变按钮的相对位置',
+          type: 'array',
+          options: {
+            addText: '添加操作',
+            deletable: false,
+            editable: false,
+            customOptRender: visibleOpt,
+            getTitle: (item) => {
+              return item?.title;
+            },
+            onAdd: () => {
+              const defaultBtn = {
+                title: `操作项${btnsLength + 1}`,
+                id: uuid(),
+                icon: "",
+                useIcon: false,
+                showText: true,
+                dynamicHidden: true,
+                dynamicDisabled: true,
+                type: "default",
+                visible: true,
+                autoClose: true,
+                isConnected: false
+              };
+              addBtn(defaultBtn);
+              return defaultBtn;
+            },
+          },
+          value: {
+            get({ data, output }: EditorResult<Data>) {
+              initParams(data, output);
+              return data.footerBtns || [];
+            },
+            set({ data }: EditorResult<Data>, val: any[]) {
+              data.footerBtns = val;
+            }
+          }
+        },
+      ]
     }
   },
   '.ant-modal-title': {
@@ -258,7 +332,7 @@ export default {
     ]
   },
   '[data-toolbar]': {
-    title: '工具条',
+    title: '操作区',
     items: [
       {
         title: '显示',
@@ -274,7 +348,7 @@ export default {
       },
       {
         title: '对齐方式',
-        type: 'Select',
+        type: 'Radio',
         options: [
           { value: AlignEnum.FlexStart, label: '居左' },
           { value: AlignEnum.Center, label: '居中' },
@@ -288,24 +362,64 @@ export default {
             data.footerLayout = value;
           }
         }
+      },
+      {
+        title: '操作列表',
+        description: '选中拖拽各项左侧手柄，可改变按钮的相对位置',
+        type: 'array',
+        options: {
+          addText: '添加操作',
+          deletable: false,
+          editable: false,
+          customOptRender: visibleOpt,
+          getTitle: (item) => {
+            return item?.title;
+          },
+          onAdd: () => {
+            const defaultBtn = {
+              title: `操作项${btnsLength + 1}`,
+              id: uuid(),
+              icon: "",
+              useIcon: false,
+              showText: true,
+              dynamicHidden: true,
+              dynamicDisabled: true,
+              type: "default",
+              visible: true,
+              autoClose: true,
+              isConnected: false
+            };
+            addBtn(defaultBtn);
+            return defaultBtn;
+          },
+        },
+        value: {
+          get({ data, output }: EditorResult<Data>) {
+            initParams(data, output);
+            return data.footerBtns || [];
+          },
+          set({ data }: EditorResult<Data>, val: any[]) {
+            data.footerBtns = val;
+          }
+        }
       }
-      // {
-      //   title: '新增操作',
-      //   ifVisible({ data }) {
-      //     return !!data.footerBtns;
-      //   },
-      //   type: 'Button',
-      //   value: {
-      //     set({ data, input, output, slot }) {
-      //       addBtn({ data, input, output, slot });
-      //     }
-      //   }
-      // }
     ]
   },
   '[data-handler-button]': {
     title: '按钮',
     items: [
+      {
+        title: '显示',
+        type: 'Switch',
+        value: {
+          get({ data, focusArea }: EditorResult<Data>) {
+            return !!findConfig({ data, focusArea }, 'visible');
+          },
+          set({ data, focusArea }: EditorResult<Data>, value: boolean) {
+            findConfig({ data, focusArea }).visible = !!value;
+          }
+        }
+      },
       {
         title: '名称',
         type: 'Text',
@@ -319,11 +433,78 @@ export default {
         }
       },
       {
-        title: '点击',
-        type: '_event',
-        options({data, focusArea}) {
-          return {
-            outputId: findConfig({ data, focusArea }, 'id')
+        title: '基础样式',
+        items: [
+          {
+            title: '风格',
+            type: 'Select',
+            options() {
+              return [
+                { value: 'default', label: '默认' },
+                { value: 'primary', label: '主按钮' },
+                { value: 'dashed', label: '虚线按钮' },
+                { value: 'danger', label: '危险按钮' },
+                { value: 'link', label: '链接按钮' },
+                { value: 'text', label: '文字按钮' }
+              ];
+            },
+            value: {
+              get({ data, focusArea }: EditorResult<Data>) {
+                return findConfig({ data, focusArea }, 'type');
+              },
+              set({ data, focusArea }: EditorResult<Data>, value: string) {
+                findConfig({ data, focusArea }).type = value;
+              }
+            }
+          }
+        ]
+      },
+      icon('handlerButton'),
+      {
+        title: '事件',
+        items: [
+          {
+            title: '点击自动关闭对话框',
+            description: '开启时, 单击按钮会自动关闭对话框。特殊处理：当需要向外输出数据时, 对话框在数据输出后关闭。',
+            type: 'switch',
+            ifVisible({ data, focusArea }: EditorResult<Data>) {
+              return findConfig({ data, focusArea }, 'id') === 'cancel';
+            },
+            value: {
+              get({ data, focusArea }: EditorResult<Data>) {
+                return findConfig({ data, focusArea }, 'autoClose');
+              },
+              set({ data, focusArea }: EditorResult<Data>, value: boolean) {
+                findConfig({ data, focusArea }).autoClose = value;
+              }
+            }
+          },
+          {
+            title: '点击',
+            type: '_event',
+            options({data, focusArea}) {
+              return {
+                outputId: findConfig({ data, focusArea }, 'id')
+              }
+            }
+          }
+        ]
+      },
+      {
+        title: '删除',
+        type: 'Button',
+        ifVisible({ data, focusArea }) {
+          return !DefaultEvent.includes(findConfig({ data, focusArea }, 'id'));
+        },
+        value: {
+          set({ data, output, focusArea }: EditorResult<Data>) {
+            const footerBtns = data.footerBtns;
+            const itemId = findConfig({ data, focusArea }, 'id');
+            const index = footerBtns.findIndex((item) => item.id === itemId);
+            const item = data.footerBtns[index];
+
+            output.remove(item.id);
+            footerBtns.splice(index, 1);
           }
         }
       }
--- src/popup/utils.tsx
+++ src/popup/utils.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
+
 export function isEmptyString(str: string): boolean {
   if (typeof str !== 'string') {
     return false
@@ -14,3 +17,22 @@ export function uuid(pre = 'u_', len = 6) {
   }
   return pre + rtn;
 }
+
+export default ({ item, index, setList }) => {
+  return (
+    <div
+      onClick={() => {
+        setList((prev) => {
+          const copy = [...prev];
+          if (copy && copy[index]) {
+            copy[index].visible = !copy[index].visible;
+          }
+          return copy;
+        });
+      }}
+      style={{cursor: "pointer", padding: "0 10px"}}
+    >
+      {item.visible ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+    </div>
+  );
+};
