4dcdeef26adc99ae8b7148e1e58ac009a412cb75
zhuba-Ahhh <3477826311@qq.com>
2023-07-05 10:39:54 +0800

    feat(shape): add shape 1.0.0

--- src/shape/runtime.tsx
+++ src/shape/runtime.tsx
@@ -1,38 +1,31 @@
 import React from 'react';
-import { clipPaths, ShapeProps } from './constants';
+import { ShapeProps } from './constants';
 import css from './runtime.less';
-
-const wrapperStyles: React.CSSProperties = {
-  width: '100%',
-  height: '100%'
-};
 
 interface RuntimeShapeProps {
   data: ShapeProps;
 }
 
 export default function ({ data }: RuntimeShapeProps) {
-  const { type, image, rotate, size, isImgRotate } = data;
+  const { type, clipPath } = data;
 
-  const shapeStyles: React.CSSProperties = {
-    width: `${size}%`,
-    aspectRatio: 1 / 1,
-    clipPath: clipPaths[type],
-    transform: `rotate(${rotate}deg)`
-  };
-
-  const imgStyles: React.CSSProperties = {
-    top: 0,
-    left: 0,
-    objectFit: 'cover',
-    transform: `rotate(${isImgRotate ? 0 : -rotate}deg)`
-  };
+  const shapeStyles: React.CSSProperties = { };
+  
+  switch (type) {
+    case 'rectangle':
+      break;
+    case 'circle':
+      shapeStyles.borderRadius = '50%';
+      break;
+    case 'triangle':
+      shapeStyles.clipPath = clipPath || "polygon(50% 0%, 0% 100%, 100% 100%)";
+      break;
+  }
+  
 
   return (
-    <div style={wrapperStyles} className={css.wrapper}>
-      <div style={shapeStyles} className={css.shape} data-item-type="shape">
-        {image && <img src={image} alt="" style={imgStyles} />}
-      </div>
+    <div className={css.wrapper} data-item-type="wrapper">
+      <div style={shapeStyles} className={css.shape} data-item-type="shape" />
     </div>
   );
 }
