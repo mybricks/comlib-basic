99530e8c5004f89211d0dac005a3553e0047df22
tangxiaoxin <1302947749@qq.com>
2023-12-04 21:55:59 +0800

    webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -72,24 +72,6 @@ module.exports = {
   },
   externals: [
     {
-      '@mybricks/rxui': {
-        commonjs: '@mybricks/rxui',
-        commonjs2: '@mybricks/rxui',
-        amd: '@mybricks/rxui',
-        root: 'rxui'
-      },
-      'element-plus': {
-        commonjs: 'element-plus',
-        commonjs2: 'element-plus',
-        amd: 'element-plus',
-        root: 'ElementPlus'
-      },
-      vue: {
-        commonjs: 'vue',
-        commonjs2: 'vue',
-        amd: 'vue',
-        root: 'Vue'
-      },
       react: {
         commonjs: 'react',
         commonjs2: 'react',
@@ -102,67 +84,9 @@ module.exports = {
         amd: 'react-dom',
         root: 'ReactDOM'
       },
-      '@ks/yoda-js-sdk': {
-        commonjs: '@ks/yoda-js-sdk',
-        commonjs2: '@ks/yoda-js-sdk',
-        amd: '@ks/yoda-js-sdk',
-        root: 'Yoda'
-      },
       moment: 'moment',
-      'm-ui': 'm-ui',
       antd: 'antd',
-      '@m-ui/icons': 'icons',
       '@ant-design/icons': 'icons',
-      '@ant-design/charts': 'charts',
-      '@es/code-editor': 'code-editor',
-      '@formily/core': {
-        commonjs: '@formily/core',
-        commonjs2: '@formily/core',
-        amd: '@formily/core',
-        root: ['Formily', 'Core']
-      },
-      '@formily/react': {
-        commonjs: '@formily/react',
-        commonjs2: '@formily/react',
-        amd: '@formily/react',
-        root: ['Formily', 'React']
-      },
-      '@formily/reactive': {
-        commonjs: '@formily/reactive',
-        commonjs2: '@formily/reactive',
-        amd: '@formily/reactive',
-        root: ['Formily', 'Reactive']
-      },
-      '@formily/path': {
-        commonjs: '@formily/path',
-        commonjs2: '@formily/path',
-        amd: '@formily/path',
-        root: ['Formily', 'Path']
-      },
-      '@formily/shared': {
-        commonjs: '@formily/shared',
-        commonjs2: '@formily/shared',
-        amd: '@formily/shared',
-        root: ['Formily', 'Shared']
-      },
-      '@formily/reactive-react': {
-        commonjs: '@formily/reactive-react',
-        commonjs2: '@formily/reactive-react',
-        amd: '@formily/reactive-react',
-        root: ['Formily', 'ReactiveReact']
-      },
-      '@formily/json-schema': {
-        commonjs: '@formily/json-schema',
-        commonjs2: '@formily/json-schema',
-        amd: '@formily/json-schema',
-        root: ['Formily', 'JsonSchema']
-      },
-      '@formily/antd': {
-        commonjs: '@formily/antd',
-        commonjs2: '@formily/antd',
-        amd: '@formily/antd',
-        root: ['Formily', 'Antd']
-      }
     }
   ]
 }
