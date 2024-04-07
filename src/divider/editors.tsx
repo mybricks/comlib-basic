ed4ab1fdb5abc54b411e9f4a19085b432b87c096
zhuba-Ahhh <3477826311@qq.com>
2024-04-07 11:36:12 +0800

    feat(divider): complete divider

--- src/divider/editors.tsx
+++ src/divider/editors.tsx
@@ -1,4 +1,4 @@
-import { DividerProps } from "antd";
+import { DataType } from "./constants";
 
 export default {
   "@init"({ style }) {
@@ -6,101 +6,89 @@ export default {
     style.height = "fit-content";
   },
   "@resize": {
-    options: ["width"],
+    options: ["width", "height"],
   },
   ":root": {
     style: [
       {
         title: "标题",
         type: "style",
-        ifVisible({ data }: EditorResult<DividerProps>) {
+        ifVisible({ data }: EditorResult<DataType>) {
           return data.type === "horizontal";
         },
         options: [{ type: "font", config: { disableTextAlign: true } }],
         target: ".ant-divider-inner-text",
       },
       {
-        title: "标题",
+        title: "分割线",
         type: "style",
-        ifVisible({ data }: EditorResult<DividerProps>) {
+        ifVisible({ data }: EditorResult<DataType>) {
           return data.type === "horizontal";
         },
         options: ["margin"],
         target: ".ant-divider-horizontal",
       },
       {
-        title: "标题",
+        title: "分割线",
         type: "style",
-        ifVisible({ data }: EditorResult<DividerProps>) {
+        ifVisible({ data }: EditorResult<DataType>) {
           return data.type === "vertical";
         },
         options: ["margin"],
         target: ".ant-divider-vertical",
       },
-      // {
-      //   title: "线",
-      //   type: "style",
-      //   options: [{ type: "border" }],
-      //   target: ".ant-divider-horizontal.ant-divider-with-text",
-      // },
     ],
     items: [
       {
-        title: "是否虚线",
-        type: "Switch",
-        value: {
-          get({ data }: EditorResult<DividerProps>) {
-            return data?.dashed;
+        title: "分割线",
+        items: [
+          {
+            title: "是否虚线",
+            type: "Switch",
+            value: {
+              get({ data }: EditorResult<DataType>) {
+                return data?.dashed;
+              },
+              set({ data }: EditorResult<DataType>, value: DataType["dashed"]) {
+                data.dashed = value;
+              },
+            },
           },
-          set(
-            { data }: EditorResult<DividerProps>,
-            value: DividerProps["dashed"]
-          ) {
-            data.dashed = value;
+          {
+            title: "方向",
+            type: "Select",
+            options: [
+              { value: "horizontal", label: "水平" },
+              { value: "vertical", label: "垂直" },
+            ],
+            value: {
+              get({ data }: EditorResult<DataType>) {
+                return data.type;
+              },
+              set({ data }: EditorResult<DataType>, value: DataType["type"]) {
+                data.type = value;
+              },
+            },
           },
-        },
-      },
-      {
-        title: "方向",
-        type: "Select",
-        options: [
-          { value: "horizontal", label: "水平" },
-          { value: "vertical", label: "垂直" },
+          {
+            title: "颜色",
+            type: "colorPicker",
+            value: {
+              get({ data }: EditorResult<DataType>) {
+                return data.style?.borderTopColor;
+              },
+              set({ data }: EditorResult<DataType>, value: string) {
+                const newStyle = { ...data.style };
+                newStyle.borderTopColor = value;
+                data.style = newStyle;
+              },
+            },
+          },
         ],
-        value: {
-          get({ data }: EditorResult<DividerProps>) {
-            return data.type;
-          },
-          set(
-            { data, style }: EditorResult<DividerProps>,
-            value: DividerProps["type"]
-          ) {
-            if (data.type === 'vertical') {
-              style.width = 'fit-content';
-            } else {
-              style.width = '100%';
-            }
-            data.type = value;
-          },
-        },
-      },
-      {
-        title: "颜色",
-        type: "colorPicker",
-        value: {
-          get({ data }: EditorResult<DividerProps>) {
-            return data.style?.["border-top-color"];
-          },
-          set({ data }: EditorResult<DividerProps>, value: string) {
-            const newStyle = { ...data.style };
-            newStyle.borderTopColor = value;
-            data.style = newStyle;
-          },
-        },
       },
       {
         title: "标题",
-        ifVisible({ data }: EditorResult<DividerProps>) {
+        ifVisible({ data }: EditorResult<DataType>) {
           return data.type === "horizontal";
         },
         items: [
@@ -108,10 +96,10 @@ export default {
             title: "标题",
             type: "Text",
             value: {
-              get({ data }: EditorResult<DividerProps>) {
+              get({ data }: EditorResult<DataType>) {
                 return data?.children;
               },
-              set({ data }: EditorResult<DividerProps>, value: string) {
+              set({ data }: EditorResult<DataType>, value: string) {
                 data.children = value;
               },
             },
@@ -125,12 +113,12 @@ export default {
               { value: "right", label: "右" },
             ],
             value: {
-              get({ data }: EditorResult<DividerProps>) {
+              get({ data }: EditorResult<DataType>) {
                 return data?.orientation;
               },
               set(
-                { data }: EditorResult<DividerProps>,
-                value: DividerProps["orientation"]
+                { data }: EditorResult<DataType>,
+                value: DataType["orientation"]
               ) {
                 data.orientation = value;
               },
@@ -140,16 +128,31 @@ export default {
             title: "与最近的左右边缘间距",
             type: "inputNumber",
             description: "标题和最近 left/right 边框之间的距离，单位 px",
-            ifVisible({ data }: EditorResult<DividerProps>) {
+            ifVisible({ data }: EditorResult<DataType>) {
               return data.orientation !== "center";
             },
             options: [{ min: -Infinity, max: Infinity, width: 120 }],
             value: {
-              get({ data }: EditorResult<DividerProps>) {
+              get({ data }: EditorResult<DataType>) {
                 return [data?.orientationMargin || 0];
               },
-              set({ data }: EditorResult<DividerProps>, value: number[]) {
+              set({ data }: EditorResult<DataType>, value: number[]) {
                 data.orientationMargin = value[0];
+              },
+            },
+          },
+          {
+            title: "自定义标题",
+            type: "Switch",
+            value: {
+              get({ data }: EditorResult<DataType>) {
+                return data?.customizableTitle;
+              },
+              set(
+                { data }: EditorResult<DataType>,
+                value: DataType["customizableTitle"]
+              ) {
+                data.customizableTitle = value;
               },
             },
           },
--- src/divider/runtime.tsx
+++ src/divider/runtime.tsx
@@ -1,18 +1,41 @@
-import React from "react";
-import { Divider, DividerProps } from "antd";
+import React, { useCallback, useEffect, useState } from "react";
+import { Divider } from "antd";
+import { DataType } from "./constants";
 
-export default function ({ data }: RuntimeParams<DividerProps>) {
-  const { type, dashed, style, orientation, children, orientationMargin } = data;
+export default function ({ data, slots }: RuntimeParams<DataType>) {
+  const {
+    type,
+    dashed,
+    style,
+    orientation,
+    children,
+    orientationMargin,
+    customizableTitle,
+  } = data;
+
+  const [renderedChildren, setRenderedChildren] =
+    useState<React.ReactNode>(children);
+
+  const childrenRender = useCallback(() => {
+    if (customizableTitle) {
+      setRenderedChildren(slots["title"].render());
+    } else {
+      setRenderedChildren(children);
+    }
+  }, [customizableTitle, children]);
+
+  useEffect(() => {
+    childrenRender();
+  }, [childrenRender]);
 
   return (
     <Divider
       type={type}
       style={style}
-      // plain={true}
       dashed={dashed}
       orientation={orientation}
       orientationMargin={orientationMargin}
-      children={type === 'horizontal' && children}
+      children={type === "horizontal" && renderedChildren}
     />
   );
 }
