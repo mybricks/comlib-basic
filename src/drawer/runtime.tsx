be26925e357b5c5bb216f47d3ff1f0a485f0e9a8
Bigbig-Huang <hqy09092020@163.com>
2024-06-24 16:15:57 +0800

    feat:(popup&grid):add resize & grid up

--- src/drawer/runtime.tsx
+++ src/drawer/runtime.tsx
@@ -252,8 +252,12 @@ export default function ({ env, _env, data, slots, outputs, inputs, logger, styl
         maskClosable={data.maskClosable}
         //style={{ height: data.height !== 0 ? data.height : 800, width: data.width !== 0 ? data.width : 520 }}
         style={{ 
-          height: ['left', 'right'].includes(data.placement) ? (typeof style.height === 'number' ? style.height : data.height) : style.height - 50, 
-          width: ['top', 'bottom'].includes(data.placement) ? style.width : style.width - 50,
+          height: ['left', 'right'].includes(data.placement) 
+            ? (typeof style.height === 'number' ? style.height : data.height) 
+            : (typeof style.height === 'number' ? style.height - 50 : data.height -50), 
+          width: ['top', 'bottom'].includes(data.placement) 
+            ? (typeof style.width === 'number' ? style.width : data.width) 
+            : (typeof style.width === 'number' ? style.width - 50 : data.width -50),
         }}
         getContainer={false}
       >
