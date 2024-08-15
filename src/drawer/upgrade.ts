ec49091df3ea65d7c1ebc3073fc92eb572e2f445
Bigbig-Huang <hqy09092020@163.com>
2024-08-15 21:55:50 +0800

    feat(drawer&popup):add rels

--- src/drawer/upgrade.ts
+++ src/drawer/upgrade.ts
@@ -152,5 +152,18 @@ export default function ({
        width: undefined
      })
    }
+
+   /**
+   * @description 1.0.31->1.0.32  补齐rels
+  */
+   if (!output.get("setTitleDone")) {
+    output.add("setTitleDone", '修改标题完成', {type: "string"});
+    }
+    if (output.get("setTitleDone") &&
+      input.get("setTitle") &&
+      !input.get("setTitle")?.rels?.includes("setTitleDone")) {
+      input.get("setTitle").setRels(["setTitleDone"]);
+    }
+
   return true;
 }
