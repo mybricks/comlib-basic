2152367625ebf44df0a7b7462bf0d90be52225a2
leon-js <42924541+leon-js@users.noreply.github.com>
2025-07-06 11:45:54 +0800

    update

--- webpack.common.js
+++ webpack.common.js
@@ -66,37 +66,4 @@ module.exports = {
       }
     ]
   },
-  // externals: [
-  //   {
-  //     react: {
-  //       commonjs: 'react',
-  //       commonjs2: 'react',
-  //       amd: 'react',
-  //       root: 'React'
-  //     },
-  //     'react-dom': {
-  //       commonjs: 'react-dom',
-  //       commonjs2: 'react-dom',
-  //       amd: 'react-dom',
-  //       root: 'ReactDOM'
-  //     },
-  //     moment: 'moment',
-  //     antd: 'antd',
-  //     '@ant-design/icons': 'icons'
-  //   }
-  // ],
-  // externals: [
-  //   {
-  //     react: 'react',
-  //     'react-dom': 'react-dom',
-  //     antd: 'antd',
-  //     moment: 'moment',
-  //     '@ant-design/icons': '@ant-design/icons'
-  //   }
-  // ],
-  plugins: [
-    // new BundleAnalyzerPlugin({
-    //   analyzerMode: 'static'
-    // })
-  ]
 };
